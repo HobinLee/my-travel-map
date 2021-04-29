8128b4fd94bb91d34092694264ebc9baa0eeda02
HobinLee <viny5120@gmail.com>
2021-04-29 17:25:57 +0900

    merge

--- src/component/WorldMap.jsx
+++ src/component/WorldMap.jsx
@@ -22,18 +22,22 @@ const MapDiv = styled.div`
 `
 
 const WorldMap = () => {
-  //const { userList } = useSelector(state => state.map);
-  const [ country, setCountry ] = useState('Sea');
-
+  const { userListObj } = useSelector(state => state.map);
+  const [ country, setCountry ] = useState(null);
+  useEffect(()=> {
+    // const _localData = JSON.parse(window.localStorage.getItem("visited"));
+  }, [])
+  
   const generateMapGrid = () => {
     return worldgrid.map((r, i) => <MapDiv key = {i}>
      {
-       r.map((geo, j) => {
-        //if(userList.includes(geo)) {
-        //  return <Grid key={i + ','+ j} address={geo} visited={"true"} point = {country === geo} setCountry={setCountry}></Grid>
-        //} else {
+       r.filter((geo, i) => i % 10000).map((geo, j) => {
+        if(userListObj[geo]) {
+          return <Grid key={i + ','+ j} address={geo} visited={userListObj[geo]} point = {country === geo} setCountry={setCountry}></Grid>
+        } else {
           return <Grid key={i + ','+ j} address={geo} point = {country === geo} setCountry={setCountry}></Grid>
-        //}
+        }
+        
        })
      }
     </MapDiv>);
